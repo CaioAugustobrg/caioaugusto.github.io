b4c85594b6a8a2225d4571d6eabfb0a0f7d1870f
Caio Braga <caioaugustobrg@gmail.com>
2023-08-12 20:57:15 -0300

    :sparkles: feat:  to deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-//import './App.css'
+
 import GlobalStyle from './styles/global.styles'
 import Header from './components/Header'
 import AppRoutes from './components/routes'
 import Footer from './components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
